831c4c31275549cc5e82fdfa50f1eec17993f36b
NimishaCoder <shuklanimisha339@gmail.com>
2024-09-06 20:10:49 +0530

    new changes

--- Bank.cpp
+++ Bank.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+using namespace std;
+
+class Account(){
+
+
+
+};
+
+int main(){
+    
+    cout<<"*********** Welcome to The National Bank *********** "<<endl;
+    cout<<"Please Enter Your Choice: "<<endl;
+    cout<<"1.Create New Bank Account"<<endl;
+    cout<<"2.Login Into Existing Account"<<endl;
+
+
+    
+    return 0;
+}
