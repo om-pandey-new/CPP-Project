96dc66df74c937dcc6cc3f156da1ba0514a04e41
Kanha2004 <om2004pandey@gmail.com>
2024-09-19 06:55:17 +0530

    "Updated Bank.cpp: added creditCard method, modified accountServices function, and rearranged code organization; removed duplicate function definitions."

--- Bank.cpp
+++ Bank.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 class Account{
-    static int acc_no;
+    static int acc_no,cred_no;
     string acc_name;
     string mobile;
     string address;
@@ -74,12 +74,16 @@ class Account{
         cout<<"Email: "<<email<<endl;
         cout<<"------------------------------------------------------"<<endl;
 
+    }
 
+    void creditCard(){
+        cred_no=++cred_no; 
     }
 
 };
 
 int Account::acc_no = 1000;
+int Account::cred_no = 1596;
 
 class CurrAccount:public Account{
     
@@ -109,156 +113,6 @@ public:
     }
 
 };
-
-template <class T>
-void update(T& v,int& index,string& mobile,string& email,string&  address){
-
-    int up=0;
-    while(up!=4){
-        cout<<"------------------------------------------------------"<<endl;
-        cout<<"Select the  field you want to update"<<endl;
-        cout<<"1.Mobile  Number"<<endl<<"2.Email Id"<<endl<<"3.Address"<<endl<<"4.Go Back to Previous Menu"<<endl;
-        cout<<"------------------------------------------------------"<<endl;
-        cin>>up;
-        switch(up){
-            case 1:
-                cout<<"------------------------------------------------------"<<endl;
-                cout<<"Enter the new mobile number"<<endl;
-                cin>>mobile;
-                v.at(index).setMobile(mobile);
-                cout<<"Mobile Number successfully updated"<<endl;
-                cout<<"------------------------------------------------------"<<endl;
-                break;
-            case 2:
-                cout<<"------------------------------------------------------"<<endl;
-                cout<<"Enter the new email id"<<endl;
-                cin>>email;
-                v.at(index).setEmail(email);
-                cout<<"Email-Id  successfully updated"<<endl;
-                cout<<"------------------------------------------------------"<<endl;
-                break;
-            case 3:
-                cout<<"------------------------------------------------------"<<endl;
-                cout<<"Enter the new address"<<endl;
-                cin>>address;
-                v.at(index).setAddress(address);
-                cout<<"Address successfully  updated"<<endl;
-                cout<<"------------------------------------------------------"<<endl;
-                break;
-            case 4:
-                break;
-            default:
-                cout<<"Invalid Choice!! Try Again"<<endl;
-        }
-    }
-
-}
-
-template <class T>
-void registration(T& v,string& name,string& address,string& email,string& password,string& mobile){
-        v.emplace_back();
-        cout<<"Enter  The Bank Details:"<<endl;
-        cout<<"------------------------------------------------------"<<endl;
-        cout<<"Account Holder Name:"<<endl;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        getline(cin,name);
-        v.back().setAccName(name);
-        cout<<"Mobile Phone:"<<endl;
-        getline(cin,mobile);
-        v.back().setMobile(mobile);
-        cout<<"Enter Your Address:"<<endl;
-        getline(cin,address);
-        v.back().setAddress(address);
-        cout<<"Enter Your Email Id:"<<endl;
-        getline(cin,email);
-        v.back().setEmail(email);
-        cout<<"Create Your Password:"<<endl;
-        getline(cin,password);
-        v.back().setPassword(password);
-        cout << "\033[2J\033[1;1H";
-        cout<<"---Registration Successful---"<<endl;
-        cout<<"Your Account NUmber is "<<v.back().getAccNo()<<endl;
-
-}
-
-template <class T>
-void accountServices(T& v,int& index){
-        int i=0,amount,withdraw,j=0;
-        string password,mobile,address,email;
-        cout << "\033[2J\033[1;1H";
-        cout<<"------------------------------------------------------"<<endl;
-        cout<<"Choose the services you want to avail:"<<endl;//chequebook can be added later
-        cout<<"1.Know Your Details"<<endl<<"2.Deposit Amount"<<endl<<"3.Withdrawal "<<endl<<"4.Check Balance"<<endl;
-        cout<<"5.Update Details"<<endl<<"6.Card Services"<<endl<<"7.Terminate Account"<<endl<<"8.Go To  Main Menu"<<endl;
-        cout<<"------------------------------------------------------"<<endl;
-        while(i!=8){
-            cout<<"PLease Enter Your Choice:"<<endl;
-            cin>>i;
-            switch(i)
-            {
-                case 1:
-                    v.at(index).printDetails();
-                    break;
-                case 2:
-                    cout<<"Enter the amount you want to deposit:"<<endl;
-                    cin>>amount;
-                    v.at(index).setBalance(amount);
-                    cout<<"Deposit Successful"<<endl<<"Deposited  Amount is "<<amount<<endl;
-                    break;
-                case 3:
-                    cout<<"Enter the amount you want to withdraw:"<<endl;
-                    cin>>withdraw;
-                    //cout<<"Enter The Password for authorization:"<<endl;
-                    if(checkPass(v,index,password)){
-                        //if(v.at(index).getBalance()>=withdraw){
-                         v.at(index).withdrawal(withdraw);
-                         //cout<<"Transaction Successful."<<endl;
-                        //}
-                        //else cout<<"Insufficient Balance!!"<<endl;
-                    }
-                    break;
-                case 4:
-                    cout<<"Your Balance is:"<<v.at(index).getBalance()<<endl;
-                    break;
-                case 5:
-                    update(v,index,mobile,email,address);
-                    break;
-                case 6:
-                    cout<<"Choose the Type:"<<endl<<"1.Credit Card"<<endl<<"2.Debit Card"<<endl;
-                    cin>>j;
-                    switch(j)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        default:
-                            cout<<"Invalid Choice!!"<<endl;
-                            break;
-                    }
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                default:
-                    cout<<"Invalid Choice.Try Again"<<endl;
-                    break;
-            }
-        }
-
-
-}
-
-void mainMenu(void){
-    cout<<"*********** Welcome to The National Bank *********** "<<endl;
-    cout<<"1.Create New Bank Account"<<endl;
-    cout<<"2.Login Into Existing Account"<<endl;
-    cout<<"3.Other Services"<<endl;
-    cout<<"4.Contact Us"<<endl;
-    cout<<"5.Exit"<<endl;
-    cout<<"------------------------------------------------------"<<endl;
-}
 
 template <class T>
 bool checkPass(T& v,int& index,string& password){
@@ -305,6 +159,186 @@ bool checkAccount(T& v,int& index,int& exist_num){
     if(flag==1) return true;
 
 }
+
+
+template <class T>
+void update(T& v,int& index,string& mobile,string& email,string&  address){
+
+    int up=0;
+    while(up!=4){
+        cout<<"------------------------------------------------------"<<endl;
+        cout<<"Select the  field you want to update"<<endl;
+        cout<<"1.Mobile  Number"<<endl<<"2.Email Id"<<endl<<"3.Address"<<endl<<"4.Go Back to Previous Menu"<<endl;
+        cout<<"------------------------------------------------------"<<endl;
+        cin>>up;
+        switch(up){
+            case 1:
+                cout<<"------------------------------------------------------"<<endl;
+                cout<<"Enter the new mobile number"<<endl;
+                cin>>mobile;
+                v.at(index).setMobile(mobile);
+                cout<<"Mobile Number successfully updated"<<endl;
+                cout<<"------------------------------------------------------"<<endl;
+                break;
+            case 2:
+                cout<<"------------------------------------------------------"<<endl;
+                cout<<"Enter the new email id"<<endl;
+                cin>>email;
+                v.at(index).setEmail(email);
+                cout<<"Email-Id  successfully updated"<<endl;
+                cout<<"------------------------------------------------------"<<endl;
+                break;
+            case 3:
+                cout<<"------------------------------------------------------"<<endl;
+                cout<<"Enter the new address"<<endl;
+                cin>>address;
+                v.at(index).setAddress(address);
+                cout<<"Address successfully  updated"<<endl;
+                cout<<"------------------------------------------------------"<<endl;
+                break;
+            case 4:
+                break;
+            default:
+                cout<<"Invalid Choice!! Try Again"<<endl;
+        }
+    }
+}
+
+template <class T>
+void registration(T& v,string& name,string& address,string& email,string& password,string& mobile){
+        v.emplace_back();
+        cout<<"Enter  The Bank Details:"<<endl;
+        cout<<"------------------------------------------------------"<<endl;
+        cout<<"Account Holder Name:"<<endl;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        getline(cin,name);
+        v.back().setAccName(name);
+        cout<<"Mobile Phone:"<<endl;
+        getline(cin,mobile);
+        v.back().setMobile(mobile);
+        cout<<"Enter Your Address:"<<endl;
+        getline(cin,address);
+        v.back().setAddress(address);
+        cout<<"Enter Your Email Id:"<<endl;
+        getline(cin,email);
+        v.back().setEmail(email);
+        cout<<"Create Your Password:"<<endl;
+        getline(cin,password);
+        v.back().setPassword(password);
+        cout << "\033[2J\033[1;1H";
+        cout<<"---Registration Successful---"<<endl;
+        cout<<"Your Account NUmber is "<<v.back().getAccNo()<<endl;
+
+}
+
+template <class T>
+void transfer(T& v,int& index2){
+    int amount;
+    cout<<"Enter The Amount You want to transfer:"<<endl;
+    cin>>amount;
+    v.at(index2).setBalance(amount);
+}
+
+template <class T>
+void accountServices(T& v,int& index){
+        int i=0,amount,withdraw,j=0,k=0,l=0,index2=0,exist_num2;
+        string password,mobile,address,email;
+        cout << "\033[2J\033[1;1H";
+        cout<<"------------------------------------------------------"<<endl;
+        cout<<"Choose the services you want to avail:"<<endl;//chequebook can be added later
+        cout<<"1.Know Your Details"<<endl<<"2.Deposit Amount"<<endl<<"3.Withdrawal "<<"4.Transfer Money To Another Bank Account"<<endl<<"5.Check Balance"<<endl;
+        cout<<"6.Update Details"<<endl<<"7.Card Services"<<endl<<"8.Terminate Account"<<endl<<"9.Go To  Main Menu"<<endl;
+        cout<<"------------------------------------------------------"<<endl;
+        while(i!=9){
+            cout<<"PLease Enter Your Choice:"<<endl;
+            cin>>i;
+            switch(i)
+            {
+                case 1:
+                    v.at(index).printDetails();
+                    break;
+                case 2:
+                    cout<<"Enter the amount you want to deposit:"<<endl;
+                    cin>>amount;
+                    v.at(index).setBalance(amount);
+                    cout<<"Deposit Successful"<<endl<<"Deposited  Amount is "<<amount<<endl;
+                    break;
+                case 3:
+                    cout<<"Enter the amount you want to withdraw:"<<endl;
+                    cin>>withdraw;
+                    //cout<<"Enter The Password for authorization:"<<endl;
+                    if(checkPass(v,index,password)) v.at(index).withdrawal(withdraw);
+                    break;
+                case 4:
+                    cout<<"Select Account Type:"<<endl<<"1.Current Account"<<endl<<"2.Savings Account"<<endl;
+                    cin>>l;
+                    switch(l)
+                    {
+                        case 1:
+                            if(checkAccount(v1,index2,exist_num2)){
+                                transfer(v1,index2);
+                            }
+                            break;
+                        case 2:
+                            break;
+                        default:
+                            break;
+                    }
+
+                case 5:
+                    cout<<"Your Balance is:"<<v.at(index).getBalance()<<endl;
+                    break;
+                case 6:
+                    update(v,index,mobile,email,address);
+                    break;
+                case 7:
+                    cout<<"Choose the Type:"<<endl<<"1.Credit Card"<<endl<<"2.Debit Card"<<endl;
+                    cin>>j;
+                    switch(j)
+                    {
+                        case 1:
+                            cout<<"------------------------------------------------------"<<endl;
+                            cout<<"1.Apply for a New Card "<<endl<<"2.Login Into  Existing Card "<<endl;
+                            cin>>k;
+                            switch(k)
+                            {
+                                case 1:
+                                    
+                                    break;
+                                case 2:
+                                    break;   
+                            }
+                            break;
+                        case 2:
+                            break;
+                        default:
+                            cout<<"Invalid Choice!!"<<endl;
+                            break;
+                    }
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                default:
+                    cout<<"Invalid Choice.Try Again"<<endl;
+                    break;
+            }
+        }
+}
+
+void mainMenu(void){
+    cout<<"*********** Welcome to The National Bank *********** "<<endl;
+    cout<<"1.Create New Bank Account"<<endl;
+    cout<<"2.Login Into Existing Account"<<endl;
+    cout<<"3.Other Services"<<endl;
+    cout<<"4.Contact Us"<<endl;
+    cout<<"5.Exit"<<endl;
+    cout<<"------------------------------------------------------"<<endl;
+}
+
+
+
 
 //int flag1=0;
 int input=0,i,exist_num,index;//flag=0;
@@ -405,8 +439,6 @@ int main(){
             cout<<"Wrong Choice Entered"<<endl<<"Enter Again or Press 5 To Exit"<<endl;
             break;
         }
-
     }
-    
     return 0;
 }
